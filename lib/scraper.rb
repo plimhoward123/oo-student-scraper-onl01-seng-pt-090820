964bcd7c568084d44a5d65d145e7d42fbb5bbd54
Plimon Howard <plimon.howard@gmail.com>
2020-10-26 16:46:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
       end
     end
     profHash[:profile_quote] = document.css(".profile-quote").text.strip
-    profHash[:bio] = document.css(".description-holder").text.strip
+    profHash[:bio] = document.css(".description-holder/p").text.strip
     puts profHash
   end
 end
