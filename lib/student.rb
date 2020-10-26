5a08f6ed423670b719342d138bb36f64fb5860a8
Plimon Howard <plimon.howard@gmail.com>
2020-10-26 17:12:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each {|k,v| self.send(("k="),v) }
+    student_hash.each {|k,v| self.send(("#{k=}"),v) }
     @@all << self
   end
 
