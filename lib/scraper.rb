f93d84d874f6a73a38fc5d544f332baff3ab06f5
Plimon Howard <plimon.howard@gmail.com>
2020-10-26 16:29:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,22 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = []
+    document = Nokogiri::HTML(open(index_url))
+    document.css(".student-card").each do |element|
+      student_hash = { name: element.css(".student-name").text,
+        location: element.css(".student-location").text,
+        profile_url: element.css('@href').text
+        }
+        student_array << student_hash
+    end
+    return student_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    document = Nokogiri::HTML(open(profile_url))
+    document.css('@href').each do |element|
+      puts element
+    end
   end
-
 end
-
