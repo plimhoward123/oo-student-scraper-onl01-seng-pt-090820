5ed366145dc7bae1830371a8eda2eb8f32649f97
Plimon Howard <plimon.howard@gmail.com>
2020-10-26 17:13:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    student_array.each do |hash|
+      self.new(hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
